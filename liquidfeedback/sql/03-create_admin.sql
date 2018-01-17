a34a19924d7f12daa164d4c40a03a9037dbbf514
Thomas Sänger <thomas@gecko.space>
2018-01-17 22:54:40 +0100

    first draft

---
+++ liquidfeedback/sql/03-create_admin.sql
@@ -0,0 +1,1 @@
+INSERT INTO member (login, name, activated, admin, password) VALUES ('admin', 'Administrator', CURRENT_TIMESTAMP, TRUE, '$1$/EMPTY/$NEWt7XJg2efKwPm4vectc1');
---
+++ liquidfeedback/start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+/opt/moonbridge/moonbridge /opt/webmcp/bin/mcp.lua /opt/webmcp/ /opt/liquid_feedback_frontend/ main custom &
+
+PIDFILE="/var/run/lf_updated.pid"
+PID=$$
+
+if [ -f "${PIDFILE}" ] && kill -CONT $( cat "${PIDFILE}" ); then
+    echo "lf_updated is already running."
+    exit 1
+fi
+
+echo "${PID}" > "${PIDFILE}"
+
+# LQFB_PG_CONN is set via docker
+while true; do
+    /opt/liquid_feedback_core/lf_update ${LQFB_PG_CONN} 2>&1
+    /opt/liquid_feedback_core/lf_update_issue_order ${LQFB_PG_CONN} 2>&1
+    /opt/liquid_feedback_core/lf_update_suggestion_order ${LQFB_PG_CONN} 2>&1
+    sleep 5
+done
