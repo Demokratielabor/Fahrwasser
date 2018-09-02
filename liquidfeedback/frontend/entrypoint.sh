1c294957a75615f396a023fcdb04dac85ba73f61
Thomas Sänger <thomas@gecko.space>
2018-09-02 21:46:50 +0200

    add mail-support via msmtp

--- liquidfeedback/frontend/entrypoint.sh
+++ liquidfeedback/frontend/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+echo "default: ${MAIL_TO}" > /etc/aliases
+
+sed /msmtprc.template \
+	-e "s/__MAIL_HOST__/${MAIL_HOST}/g" \
+	-e "s/__MAIL_PORT__/${MAIL_PORT}/g" \
+	-e "s/__MAIL_USER__/${MAIL_USER}/g" \
+	-e "s/__MAIL_PASSWORD__/${MAIL_PASSWORD}/g" \
+	-e "s/__MAIL_FROM__/${MAIL_FROM}/g" \
+	> /etc/msmtprc
+
+
 sed /init.template.lua \
 	-e "s/__POSTGRES_HOST__/${POSTGRES_HOST}/g" \
 	-e "s/__POSTGRES_DB__/${POSTGRES_DB}/g" \
