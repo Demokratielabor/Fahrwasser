38b42ecd4c2c9820b03f2d2326023b461f75af03
Thomas Sänger <thomas@gecko.space>
2019-04-28 21:26:42 +0200

    travis-ci: fix building.

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-cd pgbouncer || exit 1
-docker build -t demolab/fahrwasser_pgbouncer:$TRAVIS_BRANCH || exit 1
+docker build -t demolab/fahrwasser_pgbouncer:$TRAVIS_BRANCH pgbouncer || exit 1
 
-cd ../pgbouncer-exporter || exit 1
-docker build -t demolab/fahrwasser_pgbouncer_exporter:$TRAVIS_BRANCH || exit 1
+docker build -t demolab/fahrwasser_pgbouncer_exporter:$TRAVIS_BRANCH pgbouncer-exporter || exit 1
 
-cd ../liquidfeedback/core || exit 1
-docker build -t demolab/fahrwasser_lqfb_core:$TRAVIS_BRANCH || exit 1
+docker build -t demolab/fahrwasser_lqfb_core:$TRAVIS_BRANCH liquidfeedback/core || exit 1
 
-cd ../frontend || exit 1
-docker build -t demolab/fahrwasser_lqfb_frontend:$TRAVIS_BRANCH || exit 1
+docker build -t demolab/fahrwasser_lqfb_frontend:$TRAVIS_BRANCH liquidfeedback/frontend || exit 1
